7d5ac3db3731e7356413d28fc40fdf6623de43bc
bielsil27@gmail.com <bielsil27@gmail.com>
2023-12-07 08:57:01 -0300

    add ordering and etag to api

--- backend/api/src/getNews.js
+++ backend/api/src/getNews.js
@@ -17,19 +17,36 @@ const tableName = process.env.AWS_DYNAMODB_TABLE_NAME || 'news';
 async function getNews () {
     try {
         const params = {
-            TableName: tableName,            
-            IndexName: 'scrape_date',
-            ScanIndexForward: false
+            TableName: tableName
         };  
 
         const scanCommand = new ScanCommand(params);
         const data = await dynamoDBClient.send(scanCommand);
+        const orderedData = orderData(data.Items);
+
         console.log('success');
-        return { statusCode: 200, body: data.Items };
+        return { 
+            statusCode: 200, 
+            body: data.Items, 
+            headers: {
+                'ETag': `"${orderedData[0].scrape_date.S}"`,
+            }, 
+        };
     } catch (error) {
         console.log('error trying to get news: ' + error);
         return { statusCode: 500, body: [] };
     }
+}
+
+function orderData(items) {
+    const sortedItems = items.sort((a, b) => {
+        const aDate = new Date(a.scrape_date.S);
+        const bDate = new Date(b.scrape_date.S);
+      
+        return bDate - aDate;
+    });
+
+    return sortedItems;
 }
 
 module.exports = {
